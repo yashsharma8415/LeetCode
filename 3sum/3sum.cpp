702a29e726cb506b90a2eaebd5bf31886e8d09ca
Yash Sharma <yashsharma8415@gmail.com>
2021-07-16 15:29:30 +0530

    Time: 76 ms (70.32%), Space: 20 MB (66.72%) - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -3,31 +3,42 @@ public:
     vector<vector<int>> threeSum(vector<int>& num)
     {
         vector<vector<int>> ans;
-        set<vector<int>> anss;
-        
-        if(num.size() <= 2)
+
+        if (num.size() <= 2)
             return ans;
 
         sort(num.begin(), num.end());
 
+        int reqSum, l, r;
+
         for (int i = 0; i < num.size() - 2; ++i)
         {
-            int reqSum = 0 - num[i], l = i + 1, r = num.size() - 1;
+            if (i > 0 && num[i] == num[i - 1])
+                continue;
+
+            reqSum = 0 - num[i], l = i + 1, r = num.size() - 1;
 
             while (l < r)
             {
                 if (num[l] + num[r] == reqSum)
-                    anss.insert({num[i], num[l], num[r]});
+                {
+                    ans.push_back({num[i], num[l], num[r]});
 
-                if (num[l] + num[r] <= reqSum)
+                    l++;
+                    r--;
+
+                    while (l < r && num[l] == num[l - 1])
+                        l++;
+
+                    while (l < r && num[r] == num[r + 1])
+                        r--;
+                }
+                else if (num[l] + num[r] <= reqSum)
                     l++;
                 else
                     r--;
             }
         }
-
-        for (auto ele : anss)
-            ans.push_back(ele);
 
         return ans;
     }
